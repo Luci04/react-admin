ecb1304542fb52a3dcd4766b6dea1bb1f56eb763
Avinash Shukla <avinashshukla.mobilefirst@gmail.com>
2023-03-07 17:32:13 +0530

    Top Bar Added

--- src/screen/global/Sidebar.js
+++ src/screen/global/Sidebar.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { ProSidebar as Sidebar, Menu, MenuItem } from 'react-pro-sidebar'
+import { Box, IconButton, Typography, useTheme } from '@mui/material'
+import { Link } from 'react-router-dom';
+import { tokens } from '../../theme';
+import { LightModeOutlined } from '@mui/icons-material';
 
 export const Sidebar = () => {
     return (
